a90990706f50ddd25222951e75a5d81e41922f5f
Álvaro <ainiguezdislaa@gmail.com>
2023-03-06 16:43:20 +0100

    ej

--- src/ej9.java
+++ src/ej9.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class ej9 {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ej9 {
+    public static int suma(int n) {
+        try{
+            if (n < 0) {
+                throw new IOException("El número debe ser mayor que 0");
+            } else if (n == 0) {
+                return 0;
+            } else {
+                return n + suma(n - 1);
+            }
+        }catch ( Exception e ){
+            System.out.println("Error: " + e);
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        int n = sc.nextInt();
+        System.out.println(suma(n));
+    }
+
 }
